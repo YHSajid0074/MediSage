4fa301735699a547d85ddfa06e169ab2c2026830
Sajid <yeamimhossainsajid@gmail.com>
2025-03-24 10:19:57 +0600

    AI added

---
+++ src/main/java/com/example/MediSage/config/ai/controller/AskCvController.java
@@ -0,0 +1,21 @@
+package com.example.MediSage.config.ai.controller;
+
+import com.example.MediSage.config.ai.service.AskCvService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ai")
+public class AskCvController {
+    AskCvService askCvService;
+    public AskCvController(AskCvService askCvService) {
+        this.askCvService = askCvService;
+    }
+    @GetMapping("cv/question/{id}/{text}")
+    public ResponseEntity<String> askCv(@PathVariable Long id,@PathVariable String text) {
+        return ResponseEntity.ok(askCvService.askCv(id, text));
+    }
+}
---
+++ src/main/java/com/example/MediSage/config/ai/service/AskCvService.java
@@ -0,0 +1,26 @@
+package com.example.MediSage.config.ai.service;
+
+
+import com.example.MediSage.auth.model.User;
+import com.example.MediSage.auth.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AskCvService {
+    @Autowired
+    UserRepo userRepo;
+    @Autowired
+    CohereService cohereService;
+//    @Autowired
+//    OpenRouterService openRouterService;
+
+
+    public String askCv(Long id,String question) {
+        User user = userRepo.findById(id).get();
+//        String cv=user.getCandidate().getCvInText();
+//        String answer = cohereService.askCreator(cv+"         bro just read the full above text and think that it is you and based on that try to answer the below question.                write within 2 lines and fast and straight answer.no need additional talking  "+question);
+        return null;
+    }
+
+}
