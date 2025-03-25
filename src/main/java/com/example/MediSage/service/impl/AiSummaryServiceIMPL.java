1613133af4d5a8f861cc9a5d1fcb7c1d2f5b01ee
Sajid <yeamimhossainsajid@gmail.com>
2025-03-25 22:41:27 +0600

    AiSummery and AppointmentService Updated

--- src/main/java/com/example/MediSage/service/impl/AiSummaryServiceIMPL.java
+++ src/main/java/com/example/MediSage/service/impl/AiSummaryServiceIMPL.java
@@ -6,7 +6,10 @@ import com.example.MediSage.generic.payload.response.BaseResponseDto;
 import com.example.MediSage.generic.repository.AbstractRepository;
 import com.example.MediSage.generic.service.AbstractService;
 import com.example.MediSage.payload.request.AiSummeryRequestDTO;
+import com.example.MediSage.payload.response.AiSummeryResponseDTO;
+import com.example.MediSage.repository.patients.PatientsRepository;
 import com.example.MediSage.service.AiSummaryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -14,23 +17,34 @@ import java.io.IOException;
 
 @Service
 public class AiSummaryServiceIMPL extends AbstractService<AiSummary, AiSummeryRequestDTO, GenericSearchDto> implements AiSummaryService {
+
+    @Autowired
+    PatientsRepository patientsRepository;
+
     public AiSummaryServiceIMPL(AbstractRepository<AiSummary> repository) {
         super(repository);
     }
 
     @Override
-    protected <T extends BaseResponseDto> T convertToResponseDto(AiSummary aiSummary) {
-        return null;
+    protected AiSummeryResponseDTO convertToResponseDto(AiSummary aiSummary) {
+        AiSummeryResponseDTO responseDto = new AiSummeryResponseDTO();
+        responseDto.setSummaryText(aiSummary.getSummaryText());
+        responseDto.setPatient(aiSummary.getPatient());
+        return responseDto;
     }
 
     @Override
     protected AiSummary convertToEntity(AiSummeryRequestDTO aiSummeryRequestDTO) throws IOException {
-        return null;
+        return updateEntity(aiSummeryRequestDTO, new AiSummary());
     }
 
     @Override
     protected AiSummary updateEntity(AiSummeryRequestDTO aiSummeryRequestDTO, AiSummary entity) throws IOException {
-        return null;
+
+        entity.setSummaryText(aiSummeryRequestDTO.getSummaryText());
+        entity.setPatient(patientsRepository.findById(aiSummeryRequestDTO.getPatientId()).get());
+
+        return entity;
     }
 
     @Override
--- src/main/java/com/example/MediSage/service/impl/AppointmentServiceIMPL.java
+++ src/main/java/com/example/MediSage/service/impl/AppointmentServiceIMPL.java
@@ -6,7 +6,12 @@ import com.example.MediSage.generic.payload.response.BaseResponseDto;
 import com.example.MediSage.generic.repository.AbstractRepository;
 import com.example.MediSage.generic.service.AbstractService;
 import com.example.MediSage.payload.request.AppointmentRequestDTO;
+import com.example.MediSage.payload.response.AppointmentResponseDTO;
+import com.example.MediSage.repository.doctor.DoctorRepository;
+import com.example.MediSage.repository.hospital.HospitalRepository;
+import com.example.MediSage.repository.patients.PatientsRepository;
 import com.example.MediSage.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -14,23 +19,54 @@ import java.io.IOException;
 
 @Service
 public class AppointmentServiceIMPL extends AbstractService<Appointment, AppointmentRequestDTO, GenericSearchDto> implements AppointmentService {
+
+    @Autowired
+    PatientsRepository patientsRepository;
+
+  @Autowired
+
+  HospitalRepository hospitalRepository;
+
+  @Autowired
+  DoctorRepository doctorRepository;
+
     public AppointmentServiceIMPL(AbstractRepository<Appointment> repository) {
         super(repository);
     }
 
     @Override
-    protected <T extends BaseResponseDto> T convertToResponseDto(Appointment appointment) {
-        return null;
+    protected AppointmentResponseDTO convertToResponseDto(Appointment appointment) {
+
+        AppointmentResponseDTO appointmentResponseDTO = new AppointmentResponseDTO();
+
+        appointmentResponseDTO.setAppointmentDate(appointment.getAppointmentDate());
+        appointmentResponseDTO.setStatus(appointment.getStatus());
+        appointmentResponseDTO.setAiSummary(appointment.getAiSummary());
+        appointmentResponseDTO.setNotes(appointment.getNotes());
+        appointmentResponseDTO.setPatient(appointment.getPatient());
+        appointmentResponseDTO.setDoctor(appointment.getDoctor());
+        appointmentResponseDTO.setPrescription(appointment.getPrescription());
+
+        return appointmentResponseDTO;
     }
 
     @Override
     protected Appointment convertToEntity(AppointmentRequestDTO appointmentRequestDTO) throws IOException {
-        return null;
+        return updateEntity(appointmentRequestDTO, new Appointment());
     }
 
     @Override
     protected Appointment updateEntity(AppointmentRequestDTO appointmentRequestDTO, Appointment entity) throws IOException {
-        return null;
+
+        entity.setAppointmentDate(appointmentRequestDTO.getAppointmentDate());
+        entity.setStatus(appointmentRequestDTO.getStatus());
+        entity.setAiSummary(appointmentRequestDTO.getAiSummary());
+        entity.setNotes(appointmentRequestDTO.getNotes());
+        entity.setPrescription(appointmentRequestDTO.getPrescription());
+        entity.setDoctor(doctorRepository.findById(appointmentRequestDTO.getDoctorId()).get());
+        entity.setPatient(patientsRepository.findById(appointmentRequestDTO.getPatientId()).get());
+
+        return entity;
     }
 
     @Override
